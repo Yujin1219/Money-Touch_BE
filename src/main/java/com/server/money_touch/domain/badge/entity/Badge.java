ef533770d267b048027c34edd58634a879d02300
Yujin1219 <jin-1219@naver.com>
2025-07-06 13:07:40 +0900

    [#9] Feat: 배지 관련 엔터티 설정

---
+++ src/main/java/com/server/money_touch/domain/badge/entity/Badge.java
@@ -0,0 +1,25 @@
+package com.server.money_touch.domain.badge.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+
+}
