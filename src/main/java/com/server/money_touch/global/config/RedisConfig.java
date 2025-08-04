7e36ea786f60aa71a52901f119ff5bc4f517b4cd
mmije0ng <victon777@naver.com>
2025-08-04 15:40:09 +0900

    [#111] CI: Redis 설정 추가

---
+++ src/main/java/com/server/money_touch/global/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.server.money_touch.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    // 레디스 캐시
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
--- src/main/java/com/server/money_touch/global/test/TestController.java
+++ src/main/java/com/server/money_touch/global/test/TestController.java
@@ -2,7 +2,9 @@ package com.server.money_touch.global.test;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -10,9 +12,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private final RedisTemplate<String, String> stringRedisTemplate;
+
     @GetMapping("/test")
     public String test() {
         return "test";
     }
 
+    @GetMapping("/test-redis/set")
+    public String testRedis(@RequestParam String key, @RequestParam String value) {
+        try {
+            // Redis에 데이터 저장
+            stringRedisTemplate.opsForValue().set(key, value);
+
+            // Redis에서 데이터 가져오기
+            String redisValue = stringRedisTemplate.opsForValue().get(key);
+
+            return redisValue != null ? "Redis 연결 성공! Value: " + redisValue : "Redis 연결 실패!";
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return "Redis 오류: " + e.getMessage();
+        }
+    }
+
+    @GetMapping("/test-redis/exist")
+    public String checkKey(@RequestParam String key) {
+        try {
+            // Redis에 해당 키가 존재하는지 확인
+            Boolean exists = stringRedisTemplate.hasKey(key);
+
+            // 결과 반환
+            if (exists != null && exists) {
+                return "키가 Redis에 존재합니다: " + key;
+            } else {
+                return "키가 Redis에 존재하지 않습니다: " + key;
+            }
+        } catch (Exception e) {
+            log.error("Redis 오류: ", e);
+            return "Redis 오류: " + e.getMessage();
+        }
+    }
 }
