688c75ad2b80f6e313cb4522b143f2d379ba504b
paul0755 <100present@naver.com>
2025-08-11 17:16:35 +0900

    fix: 이메일 요청 시, 이메일 중복 검증 로직 추가

--- src/main/java/com/server/money_touch/domain/user/service/user/UserQueryService.java
+++ src/main/java/com/server/money_touch/domain/user/service/user/UserQueryService.java
@@ -2,6 +2,8 @@ package com.server.money_touch.domain.user.service.user;
 
 import com.server.money_touch.domain.user.dto.UserResponse;
 
+import java.io.IOException;
+
 public interface UserQueryService {
     // User 존재 여부 검증
     Boolean existsUserById(Long userId);
@@ -11,6 +13,9 @@ public interface UserQueryService {
 
     // nickname 중복 여부 검증
     Boolean existsByNickname(String nickname);
+
+    // 이메일 요청및 이메일 중복 검증
+    void requestEmailVerification(String toEmail)throws IOException;
 
     // 마이페이지 유저 정보 조회
     UserResponse.MyPageResponseDTO getMyPageInfo(Long userId);
