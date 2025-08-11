52c8e0f85134d7af95fb8223e84c91a46ea6a203
paul0755 <100present@naver.com>
2025-08-11 16:45:43 +0900

    feat: 닉네임 중복 검증 api 구현

--- src/main/java/com/server/money_touch/domain/user/service/user/UserQueryService.java
+++ src/main/java/com/server/money_touch/domain/user/service/user/UserQueryService.java
@@ -9,6 +9,8 @@ public interface UserQueryService {
     // email 존재 여부 검증
     Boolean existsByEmail(String email);
 
+    // nickname 중복 여부 검증
+    Boolean existsByNickname(String nickname);
 
     // 마이페이지 유저 정보 조회
     UserResponse.MyPageResponseDTO getMyPageInfo(Long userId);
