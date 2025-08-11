b267ff0fdb68dd27e26a5642bb4eadc1b42d3218
mmije0ng <victon777@naver.com>
2025-08-11 20:39:28 +0900

    [#136] Fix: 고정비 스케줄링 메서드에 distinct 추가

--- src/main/java/com/server/money_touch/domain/fixedConsumption/repository/FixedConsumptionRepository.java
+++ src/main/java/com/server/money_touch/domain/fixedConsumption/repository/FixedConsumptionRepository.java
@@ -3,11 +3,13 @@ package com.server.money_touch.domain.fixedConsumption.repository;
 import com.server.money_touch.domain.fixedConsumption.entity.FixedConsumption;
 import com.server.money_touch.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FixedConsumptionRepository extends JpaRepository<FixedConsumption, Long>, FixedConsumptionRepositoryCustom {
+    @Query("select distinct fc from FixedConsumption fc where fc.user = :user")
     List<FixedConsumption> findAllByUser(User user);
 
     Optional<FixedConsumption> findByIdAndUserId(Long id, Long userId);
